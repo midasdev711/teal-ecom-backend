3fbc187bb1692d3d0bad76c1cd3c91765f848caf
Vimal Tagline <vimal.tagline@gmail.com>
2020-10-21 12:06:41 +0530

    Domain name changes

--- news/schema/constant.js
+++ news/schema/constant.js
@@ -13,7 +13,7 @@
 //     "Password" :'Solace@2020',
 //     // "HTTP_RESET_URL" : "http://localhost/demo/node_mail.html"
 //     // "HTTP_RESET_URL" : "http://localhost:3000/resetpassword"
-//     "HTTP_RESET_URL" : "https://teal.com/resetpassword"
+//     "HTTP_RESET_URL" : "https://juicypie.com/resetpassword"
 // };
 
 const MAIL_DETAILS = {
@@ -23,7 +23,7 @@ const MAIL_DETAILS = {
   "Password" :'account@321',
   // "HTTP_RESET_URL" : "http://localhost/demo/node_mail.html"
   // "HTTP_RESET_URL" : "http://localhost:3000/resetpassword"
-  "HTTP_RESET_URL" : "https://teal.com/resetpassword"
+  "HTTP_RESET_URL" : "https://juicypie.com/resetpassword"
 };
 
 
@@ -39,7 +39,7 @@ const AWSCredentails = {
     ContentType: 'image/png',
     ACL: "public-read-write",
     // AWS_BASE_URL : "https://teal.s3.us-east-2.amazonaws.com/",
-    AWS_BASE_URL : "http://cdn.teal.com/",
+    AWS_BASE_URL : "http://cdn.juicypie.com/",
     AWS_USER_IMG_PATH : "users",
     AWS_SHOP_IMG_PATH : "shop",
     AWS_STORIES_IMG_PATH : "stories",
@@ -56,7 +56,7 @@ const LocalFolderImagePath = 'images',
       PremiumContentLen = 100,
       TitleMaxLen = 125,
       SubTitleMaxLen = 150,
-      SubscribeCdnUrl = "http://cdn.teal.com/subscribe.jpg",
+      SubscribeCdnUrl = "http://cdn.juicypie.com/subscribe.jpg",
       TOKEN_SECRET_KEY = "5myg3NmusbIH1pSUhsnhTM33aa5rqR4d";
 
   const ConstantArray = { TOKEN_SECRET_KEY,SubTitleMaxLen, TitleMaxLen,AWSCredentails,SubscribeCdnUrl,PremiumContentLen,AmountType, MAIL_DETAILS,LocalFolderImagePath,UserLocalImagePath, ArticleStatusConst,RoleObject,DonationStatusConst };
